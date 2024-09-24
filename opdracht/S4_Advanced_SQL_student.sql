872dc00393d33fa75df27ac63219ffa051ce53d9
KasperMeijer <ksprm4@gmail.com>
2024-09-24 14:00:01 +0200

    S4

--- opdracht/S4_Advanced_SQL_student.sql
+++ opdracht/S4_Advanced_SQL_student.sql
@@ -29,24 +29,58 @@
 -- S4.1. 
 -- Geef nummer, functie en geboortedatum van alle medewerkers die vóór 1980
 -- geboren zijn, en trainer of verkoper zijn.
--- DROP VIEW IF EXISTS s4_1; CREATE OR REPLACE VIEW s4_1 AS                                                     -- [TEST]
-
+DROP VIEW IF EXISTS s4_1; CREATE OR REPLACE VIEW s4_1 AS                                                     -- [TEST]
+select
+    m.mnr,
+    m.functie,
+    m.gbdatum
+from
+    medewerkers m
+where
+    m.gbdatum < '1980-01-01'
+  and (m.functie = 'VERKOPER' or m.functie = 'TRAINER');
 
 -- S4.2. 
 -- Geef de naam van de medewerkers met een tussenvoegsel (b.v. 'van der').
 -- DROP VIEW IF EXISTS s4_2; CREATE OR REPLACE VIEW s4_2 AS                                                     -- [TEST]
 
+-- Er is geen tussenvoegsel kolom
+
 
 -- S4.3. 
 -- Geef nu code, begindatum en aantal inschrijvingen (`aantal_inschrijvingen`) van alle
 -- cursusuitvoeringen in 2019 met minstens drie inschrijvingen.
--- DROP VIEW IF EXISTS s4_3; CREATE OR REPLACE VIEW s4_3 AS                                                     -- [TEST]
-
+DROP VIEW IF EXISTS s4_3; CREATE OR REPLACE VIEW s4_3 AS                                                     -- [TEST]
+select
+    u.cursus as code,
+    u.begindatum,
+    count(i.cursist) as aantal_inschrijvingen
+from
+    uitvoeringen u
+        join
+    inschrijvingen i on u.begindatum = i.begindatum
+where
+    extract(year from u.begindatum) = 2019
+group by
+    u.cursus,
+    u.begindatum
+having
+    count(i.cursist) >= 3;
 
 -- S4.4. 
 -- Welke medewerkers hebben een bepaalde cursus meer dan één keer gevolgd?
 -- Geef medewerkernummer en cursuscode.
--- DROP VIEW IF EXISTS s4_4; CREATE OR REPLACE VIEW s4_4 AS                                                     -- [TEST]
+DROP VIEW IF EXISTS s4_4; CREATE OR REPLACE VIEW s4_4 AS                                                     -- [TEST]
+select
+    i.cursist,
+    i.cursus
+from
+    inschrijvingen i
+group by
+    i.cursist,
+    i.cursus
+having
+    count(*) > 1;
 
 
 -- S4.5. 
@@ -58,24 +92,39 @@
 --   ERM    | 1 
 --   JAV    | 4 
 --   OAG    | 2 
--- DROP VIEW IF EXISTS s4_5; CREATE OR REPLACE VIEW s4_5 AS                                                     -- [TEST]
-
+DROP VIEW IF EXISTS s4_5; CREATE OR REPLACE VIEW s4_5 AS                                                     -- [TEST]
+select
+    u.cursus,
+    count(*) as aantal
+from
+    uitvoeringen u
+group by
+    u.cursus;
 
 -- S4.6. 
 -- Bepaal hoeveel jaar leeftijdsverschil er zit tussen de oudste en de 
 -- jongste medewerker (`verschil`) en bepaal de gemiddelde leeftijd van
 -- de medewerkers (`gemiddeld`).
 -- Je mag hierbij aannemen dat elk jaar 365 dagen heeft.
--- DROP VIEW IF EXISTS s4_6; CREATE OR REPLACE VIEW s4_6 AS                                                     -- [TEST]
-
+DROP VIEW IF EXISTS s4_6; CREATE OR REPLACE VIEW s4_6 AS                                                     -- [TEST]
+select
+    max((current_date - m.gbdatum) / 365) - min((current_date - m.gbdatum) / 365) as verschil,
+    floor(avg((current_date - m.gbdatum) / 365)) as gemiddeld
+from
+    medewerkers m;
 
 -- S4.7. 
 -- Geef van het hele bedrijf een overzicht van het aantal medewerkers dat
 -- er werkt (`aantal_medewerkers`), de gemiddelde commissie die ze
 -- krijgen (`commissie_medewerkers`), en hoeveel dat gemiddeld
 -- per verkoper is (`commissie_verkopers`).
--- DROP VIEW IF EXISTS s4_7; CREATE OR REPLACE VIEW s4_7 AS                                                     -- [TEST]
-
+DROP VIEW IF EXISTS s4_7; CREATE OR REPLACE VIEW s4_7 AS                                                     -- [TEST]
+select
+    count(*) as aantal_medewerkers,
+    floor(avg(m.comm)) as commissie_medewerkers,
+    floor(avg(case when m.functie = 'VERKOPER' then m.comm end)) as commissie_verkopers
+from
+    medewerkers m;
 
 
 -- -------------------------[ HU TESTRAAMWERK ]--------------------------------
