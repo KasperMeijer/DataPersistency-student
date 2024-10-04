8e274d7dbe9cc3a4c6be2bbd280dabc39be84816
KasperMeijer <ksprm4@gmail.com>
2024-10-04 10:26:21 +0200

    S5

--- opdracht/S5_Subqueries_student.sql
+++ opdracht/S5_Subqueries_student.sql
@@ -31,51 +31,80 @@
 -- S5.1.
 -- Welke medewerkers hebben zowel de Java als de XML cursus
 -- gevolgd? Geef hun personeelsnummers.
--- DROP VIEW IF EXISTS s5_1; CREATE OR REPLACE VIEW s5_1 AS                                                     -- [TEST]
-
+DROP VIEW IF EXISTS s5_1; CREATE OR REPLACE VIEW s5_1 AS                                                     -- [TEST]
+select mnr from medewerkers
+where mnr in (select cursist from inschrijvingen where cursus = 'JAV')
+  and mnr in (select cursist from inschrijvingen where cursus = 'XML')
 
 -- S5.2.
 -- Geef de nummers van alle medewerkers die niet aan de afdeling 'OPLEIDINGEN'
 -- zijn verbonden.
--- DROP VIEW IF EXISTS s5_2; CREATE OR REPLACE VIEW s5_2 AS                                                     -- [TEST]
-
+DROP VIEW IF EXISTS s5_2; CREATE OR REPLACE VIEW s5_2 AS                                                     -- [TEST]
+select mnr from medewerkers
+where afd not in (select anr from afdelingen where naam = 'OPLEIDINGEN')
 
 -- S5.3.
 -- Geef de nummers van alle medewerkers die de Java-cursus niet hebben
 -- gevolgd.
--- DROP VIEW IF EXISTS s5_3; CREATE OR REPLACE VIEW s5_3 AS                                                     -- [TEST]
-
+DROP VIEW IF EXISTS s5_3; CREATE OR REPLACE VIEW s5_3 AS                                                     -- [TEST]
+select mnr from medewerkers
+where mnr not in (select cursist from inschrijvingen where cursus = 'JAV')
 
 -- S5.4.
 -- a. Welke medewerkers hebben ondergeschikten? Geef hun naam.
--- DROP VIEW IF EXISTS s5_4a; CREATE OR REPLACE VIEW s5_4a AS                                                   -- [TEST]
+DROP VIEW IF EXISTS s5_4a; CREATE OR REPLACE VIEW s5_4a AS                                                   -- [TEST]
+select naam from medewerkers
+where mnr in (select distinct chef from medewerkers)
 
 -- b. En welke medewerkers hebben geen ondergeschikten? Geef wederom de naam.
--- DROP VIEW IF EXISTS s5_4b; CREATE OR REPLACE VIEW s5_4b AS                                                   -- [TEST]
+DROP VIEW IF EXISTS s5_4b; CREATE OR REPLACE VIEW s5_4b AS                                                   -- [TEST]
+select naam from medewerkers
+where mnr not in (select distinct chef from medewerkers where chef is not null)
 
 
 -- S5.5.
 -- Geef cursuscode en begindatum van alle uitvoeringen van programmeercursussen
 -- ('BLD') in 2020.
--- DROP VIEW IF EXISTS s5_5; CREATE OR REPLACE VIEW s5_5 AS                                                     -- [TEST]
-
+DROP VIEW IF EXISTS s5_5; CREATE OR REPLACE VIEW s5_5 AS                                                     -- [TEST]
+select cursus, begindatum from uitvoeringen
+where cursus in (select code from cursussen WHERE type = 'BLD')
+  and extract(year from begindatum) = 2020;
 
 -- S5.6.
 -- Geef van alle cursusuitvoeringen: de cursuscode, de begindatum en het
 -- aantal inschrijvingen (`aantal_inschrijvingen`). Sorteer op begindatum.
--- DROP VIEW IF EXISTS s5_6; CREATE OR REPLACE VIEW s5_6 AS                                                     -- [TEST]
-
+DROP VIEW IF EXISTS s5_6; CREATE OR REPLACE VIEW s5_6 AS                                                     -- [TEST]
+select distinct cursus, begindatum,
+                (select count(*)
+                 from inschrijvingen i
+                 where i.cursus = u.cursus and i.begindatum = u.begindatum) as aantal_inschrijvingen
+from uitvoeringen u
+order by begindatum
 
 -- S5.7.
 -- Geef voorletter(s) en achternaam van alle trainers die ooit tijdens een
 -- algemene ('ALG') cursus hun eigen chef als cursist hebben gehad.
--- DROP VIEW IF EXISTS s5_7; CREATE OR REPLACE VIEW s5_7 AS                                                     -- [TEST]
-
+DROP VIEW IF EXISTS s5_7; CREATE OR REPLACE VIEW s5_7 AS                                                     -- [TEST]
+select distinct t.voorl, t.naam
+from medewerkers t
+where t.mnr in (
+    select u.docent
+    from uitvoeringen u
+             join inschrijvingen i on u.cursus = i.cursus and u.begindatum = i.begindatum
+    where u.cursus in (select code from cursussen WHERE type = 'ALG')
+      and i.cursist in (select chef from medewerkers where mnr = t.mnr)
+);
 
 -- S5.8.
 -- Geef de naam van de medewerkers die nog nooit een cursus hebben gegeven.
--- DROP VIEW IF EXISTS s5_8; CREATE OR REPLACE VIEW s5_8 AS                                                     -- [TEST]
-
+DROP VIEW IF EXISTS s5_8; CREATE OR REPLACE VIEW s5_8 AS                                                     -- [TEST]
+select distinct t.naam
+from medewerkers t
+where t.mnr not in (
+    select u.docent
+    from uitvoeringen u
+             join inschrijvingen i on u.cursus = i.cursus and u.begindatum = i.begindatum
+);
 
 
 -- -------------------------[ HU TESTRAAMWERK ]--------------------------------
